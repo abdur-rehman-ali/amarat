cc30d4bfeada9e378402ae763cb26d52b095069b
abdur-rehman-ali <alijoyia911@gmail.com>
2022-05-19 21:13:41 +0500

    Add active storage for profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :profile_image
+  validates :profile_image, attached: true, content_type: ['image/png', 'image/jpeg']
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
