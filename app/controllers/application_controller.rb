3ae5677c63191c1b970b9cbd328cd2955f3d39b1
abdur-rehman-ali <alijoyia911@gmail.com>
2022-05-19 23:27:44 +0500

    Add phone number and username field to registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :profile_image)}
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :profile_image,:username,:phone)}
 
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password,:profile_image)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password,:profile_image,:username,:phone)}
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one_attached :profile_image
   validates :profile_image, attached: true, content_type: ['image/png', 'image/jpeg']
+  validates :username, presence: true
+  validates :phone, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
